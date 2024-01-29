046b53122aeb7bb2d5f7378c6a5fae144f160ed1
FloW1772 <58610562+FloW1772@users.noreply.github.com>
2024-01-29 17:26:15 +0100

    fix bug menu slider en partie

--- src/containers/Slider/index.js
+++ src/containers/Slider/index.js
@@ -12,46 +12,51 @@ const Slider = () => {
   );
   const nextCard = () => {
     setTimeout(
-      () => setIndex(index < byDateDesc.length ? index + 1 : 0),
+      () => setIndex((indexPrev) => byDateDesc && indexPrev < byDateDesc.length - 1 ? indexPrev + 1 : 0),
       5000
     );
   };
   useEffect(() => {
     nextCard();
   });
+  function changedSlide(indexRadio) {
+    console.log(indexRadio)
+  }
+
   return (
     <div className="SlideCardList">
       {byDateDesc?.map((event, idx) => (
-        <>
-          <div
-            key={event.title}
-            className={`SlideCard SlideCard--${
-              index === idx ? "display" : "hide"
+
+        <div
+          key={event.title}
+          className={`SlideCard SlideCard--${index === idx ? "display" : "hide"
             }`}
-          >
-            <img src={event.cover} alt="forum" />
-            <div className="SlideCard__descriptionContainer">
-              <div className="SlideCard__description">
-                <h3>{event.title}</h3>
-                <p>{event.description}</p>
-                <div>{getMonth(new Date(event.date))}</div>
-              </div>
+        >
+          <img src={event.cover} alt="forum" />
+          <div className="SlideCard__descriptionContainer">
+            <div className="SlideCard__description">
+              <h3>{event.title}</h3>
+              <p>{event.description}</p>
+              <div>{getMonth(new Date(event.date))}</div>
             </div>
           </div>
-          <div className="SlideCard__paginationContainer">
-            <div className="SlideCard__pagination">
-              {byDateDesc.map((_, radioIdx) => (
-                <input
-                  key={`${event.id}`}
-                  type="radio"
-                  name="radio-button"
-                  checked={idx === radioIdx}
-                />
-              ))}
-            </div>
-          </div>
-        </>
+        </div>
+
+
       ))}
+      <div className="SlideCard__paginationContainer">
+        <div className="SlideCard__pagination">
+          {byDateDesc?.map((_, radioIdx) => (
+            <input
+              key={`event-${_.title}`}
+              type="radio"
+              name="radio-button"
+              checked={index === radioIdx}
+              onChange={() => changedSlide(radioIdx)}
+            />
+          ))}
+        </div>
+      </div>
     </div>
   );
 };
